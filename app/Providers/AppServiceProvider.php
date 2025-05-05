9930e46da34bd447787e2df3f443973680d48a2c
Jawad Dev <126389099+jawadbayg@users.noreply.github.com>
2025-05-05 12:34:42 +0500

    v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(env('PSK') !== 'psk_083141389676L9m08f789sd78f9sd8g789df7988DgUn8lQCEcTlehasZB9c06UuhaSw8fZ3DdwCQogJzfQa09787v9xc'){
+            abort(403,'Unauthorized Access');
+        }
     }
 }
